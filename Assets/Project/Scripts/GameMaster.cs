2f9cd421ed82d8a4fe967172d9de08a065b2e7a0
Krzysztof Kulpa <krzysztof.kulpa.108@gmail.com>
2023-12-08 21:58:38 +0100

    main config, game master

---
+++ Assets/Project/Scripts/GameMaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Project.Scripts;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster _instance;
+    public static GameMaster Instance => _instance;
+
+    private bool _isGameLoaded;
+    private bool IsGameLoaded => Instance == null ? false : Instance._isGameLoaded;
+    [SerializeField] private MainConfig _mainConfig;
+    public static MainConfig MainConfig => _instance._mainConfig;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Load());
+    }
+
+    private IEnumerator Load()
+    {
+        yield return null;
+        _isGameLoaded = true;
+    }
+}
---
+++ Assets/Project/Scripts/MainConfig.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts
+{
+    [CreateAssetMenu(fileName = "MainConfig", menuName = "Configs/MainConfig")]
+    public class MainConfig : ScriptableObject
+    {
+        
+        
+        private static MainConfig _mainConfig;
+        public static MainConfig Instance =>
+#if !UNITY_EDITOR
+             _mainConfig ?? (_mainConfig = GameMaster.MainConfig);
+#endif
+#if UNITY_EDITOR
+            _mainConfig ? _mainConfig : Application.isEditor ? (_mainConfig = GetConfigAsset()) : GameMaster.MainConfig;
+        
+        private static MainConfig GetConfigAsset()
+        {
+            var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<MainConfig>("Assets/Project/Configs/MainConfig.asset");
+            return asset;
+        }
+#endif
+    }
+}
